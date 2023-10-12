2aead41f62592030c29739ea1849a28a4d6d38ef
Dan Brown <jdanbrown@gmail.com>
2023-10-11 22:54:38 -0600

    Fix pathToCurrentDirectory: Only use dirname on file:// uris (e.g. not magit:...)

--- src/extension.ts
+++ src/extension.ts
@@ -4,49 +4,53 @@ import * as vscode from "vscode";
 import { commands, ExtensionContext, Uri } from "vscode";
 import { AdvancedOpenFile, activeInstance } from "./advancedOpenFile";
 
-async function pathToCurrentDirectory(): Promise<string> {
+function dirForActiveEditorOrTerminal(): string | null {
   // Get dir from active text/notebook editor
   //  - activeTextEditor is undefined if there are no text editors
   //  - activeNotebookEditor is undefined if there are no notebook editors
-  const currentEditorUri =
+  const activeEditorUri =
     vscode.window.activeTextEditor?.document.uri ||
     vscode.window.activeNotebookEditor?.notebook.uri;
-  if (currentEditorUri) {
-    return Path.dirname(currentEditorUri.path);
-  }
-
-  // TODO Get cwd from terminal (which vscode tracks internally but doesn't yet expose)
+  const activeEditorDir =
+    activeEditorUri && activeEditorUri.scheme === "file"
+      ? Path.dirname(activeEditorUri.path)
+      : null;
+  // TODO Get cwd from active terminal -- which vscode tracks internally but doesn't yet expose
   //  - Need: https://github.com/microsoft/vscode/issues/145234 Expose shell integration command knowledge to extensions
   //  - Need: https://github.com/microsoft/vscode/issues/191924 Expose terminal's detected cwd to extensions
+  const activeTerminalCwd: string | null = null;
+  return activeEditorDir || activeTerminalCwd;
+}
 
+async function pathToCurrentDirectory(): Promise<string> {
+  // Get dir for active text editor / notebook editor / terminal (TODO)
+  const activeDir = dirForActiveEditorOrTerminal();
+  if (activeDir) {
+    return activeDir;
+  }
   // If no active text editors, just use the workspace dir (which falls back to the home dir)
   return await pathToCurrentWorkspace();
 }
 
 async function pathToCurrentWorkspace(): Promise<string> {
-  // Get dir from active text/notebook editor
-  //  - activeTextEditor is undefined if there are no text editors
-  //  - activeNotebookEditor is undefined if there are no notebook editors
-  const currentEditorUri =
-    vscode.window.activeTextEditor?.document.uri ||
-    vscode.window.activeNotebookEditor?.notebook.uri;
-  if (currentEditorUri) {
-    // getWorkspaceFolder returns undefined if url matches no workspace folder
-    const workspaceFolder =
-      vscode.workspace.getWorkspaceFolder(currentEditorUri);
+  // Get dir for active text editor / notebook editor / terminal (TODO)
+  const activeDir = dirForActiveEditorOrTerminal();
+  if (activeDir) {
+    const workspaceFolder = vscode.workspace.getWorkspaceFolder(
+      Uri.file(activeDir)
+    );
     if (workspaceFolder) {
       return workspaceFolder.uri.path;
     }
   }
-
-  // workspaceFolders is undefined if no workspace is opened
-  // workspaceFolders is [] if a workspace is opened but it has no folders
+  // Get dir from the first workspace folder
+  //  - workspaceFolders is undefined if no workspace is opened
+  //  - workspaceFolders is [] if a workspace is opened but it has no folders
   const workspaceFolders = vscode.workspace.workspaceFolders;
   if (workspaceFolders && workspaceFolders[0]) {
     return workspaceFolders[0].uri.path;
   }
-
-  // Nothing worked, so fall back to the user's home dir so that we always return something
+  // Nothing worked, so fall back to the user's home dir so that we always return something useful
   return os.homedir();
 }
 
